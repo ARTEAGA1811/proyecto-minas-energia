eaa38d5b535dbf8e4e00ad5a15d7dee424cceea8
Timo-Teo <timo_teo.c@hotmail.com>
2022-08-08 15:31:20 -0500

    Agregacion Footer y header
    cree la rama xd

--- src/components/Inicio.tsx
+++ src/components/Inicio.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Footer } from './Footer'
+import { Header } from './Header'
 
 const Inicio = () => {
     return (
-        <div>Inicio</div>
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
     )
 }
 
