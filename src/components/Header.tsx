e81a9fa87fded531cc4d9d4cd4e24887289cee9f
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2022-09-05 11:54:20 -0500

    Revert "feat: cambios en las rutas para el deploy"
    This reverts commit 0f1993b912f4db298bfe73d5634aa85bfa3fc052.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,6 @@ import { ModalBuscar } from './ModalBuscar'
 const Header = () => {
 
     const URLactual = window.location.pathname;
-    console.log(URLactual);
-    const subcarpeta = URLactual.split('/')[1];
-
 
     //Agrega la clase active_section al link que corresponda
     const checkURl = (currUrl: string) => {
@@ -33,13 +30,13 @@ const Header = () => {
                 <nav>
                     <ul className='nav'>
                         <li className='nav-item'>
-                            <a href="/proyecto-minas-energia" className={'nav-link mi_link ' + checkURl('/proyecto-minas-energia')}>Inicio</a>
+                            <a href="/" className={'nav-link mi_link ' + checkURl('/')}>Inicio</a>
                         </li>
                         <li className='nav-item'>
-                            <a href="/proyecto-minas-energia/temas-importantes" className={'nav-link mi_link ' + checkURl('/proyecto-minas-energia/temas-importantes')}>Temas Importantes</a>
+                            <a href="/temas-importantes" className={'nav-link mi_link ' + checkURl('/temas-importantes')}>Temas Importantes</a>
                         </li>
                         <li className='nav-item'>
-                            <a href="/proyecto-minas-energia/contacto" className={'nav-link mi_link ' + checkURl('/proyecto-minas-energia/contacto')}>Contacto</a>
+                            <a href="/contacto" className={'nav-link mi_link ' + checkURl('/contacto')}>Contacto</a>
                         </li>
                     </ul>
                 </nav>
